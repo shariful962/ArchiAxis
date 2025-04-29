874671efc569aadc104422fe9c49807704e3a8cc
Shariful <shariful0411@gmail.com>
2025-04-29 21:44:09 +0600

    google map added successfully

--- src/Pages/ContactUs/ContactUs.jsx
+++ src/Pages/ContactUs/ContactUs.jsx
@@ -15,7 +15,7 @@ const ContactUs = () => {
         every step of the way.
       </p>
 
-      <div className="mt-12">
+      <div className="mt-12 flex flex-col md:flex-row   justify-between gap-24">
         <div className="w-full md:w-1/2">
           <div className="flex flex-col md:flex-row justify-between gap-12">
             <div>
@@ -33,7 +33,7 @@ const ContactUs = () => {
               </p>
             </div>
             <div>
-            <div className="relative w-max">
+              <div className="relative w-max">
                 <h1 className="font-Inter font-semibold text-[#000000]">
                   Phone
                 </h1>
@@ -47,7 +47,7 @@ const ContactUs = () => {
               </p>
             </div>
             <div>
-            <div className="relative w-max">
+              <div className="relative w-max">
                 <h1 className="font-Inter font-semibold text-[#000000]">
                   Email
                 </h1>
@@ -61,7 +61,7 @@ const ContactUs = () => {
               </p>
             </div>
           </div>
-          <div className="mt-14">
+          <div className="mt-14 flex flex-col md:flex-row justify-between">
             <form className="grid grid-cols-1 md:grid-cols-2 gap-6">
               {/* Name */}
               <div className="flex flex-col">
@@ -154,7 +154,19 @@ const ContactUs = () => {
             </form>
           </div>
         </div>
-        <div></div>
+        <div className="w-full md:w-[45%] ">
+        <iframe
+        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3650.857066413867!2d90.42684567593547!3d23.789763187046946!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3755c73db80f4a61%3A0x3e51b86de0ff3fd1!2sKhilkhet%2C%20Dhaka!5e0!3m2!1sen!2sbd!4v1682345678901!5m2!1sen!2sbd"
+        width="100%"
+        height="80%"
+        style={{ border: 0 }}
+        allowFullScreen=""
+        loading="lazy"
+        referrerPolicy="no-referrer-when-downgrade"
+        title="Khilkhet Location"
+      ></iframe>
+        </div>
+        
       </div>
     </div>
   );
